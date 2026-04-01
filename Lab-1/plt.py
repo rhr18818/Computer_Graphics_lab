862b7458b6eef0be02df1d1adbab513ab64bce16
rhr18818 <rhr14101@gmail.com>
2026-04-01 07:18:43 +0600

    practising computer graphics hw work

--- Lab-1/plt.py
+++ Lab-1/plt.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time as t
 
+# x = [1,2,3]
+# y = [3,5,6]
+
+# plt.scatter(x,y)
+# plt.plot(x,y)
+# # plt.xlim(0,6)
+# # plt.ylim(0,6)
+
 x_list = []
 y_list =[]
 
@@ -24,6 +32,7 @@ for i in range(500):
 for i in range(500):
     #plt.scatter(x_list[i],y_list[i])
     plt.plot(x_list[i],y_list[i])
+
 
 
 plt.show()
